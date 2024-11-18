cafffa03afb8dce0ec64d6f517f9451739b7fc2a
piyushasahni <piyushasahni14@gmail.com>
2024-11-18 23:06:48 +0530

    Update customer_aggregation.sql

--- models/customer_aggregation.sql
+++ models/customer_aggregation.sql
@@ -1,22 +1,22 @@
 WITH customer_orders AS (
-SELECT
-    c.id AS customer_id,
-    c.first_name,
-    c.last_name,
-    MIN(o.order_date) AS first_order,
-    MAX(o.order_date) AS most_recent_order,
-    COUNT(o.id) AS number_of_orders
-FROM {{ source('<SCHEMA>', 'raw_customers') }} c
-LEFT JOIN {{ source('<SCHEMA>', 'raw_orders') }} o
-ON c.id = o.user_id
-GROUP BY c.id, c.first_name, c.last_name
+    SELECT
+        c.id AS customer_id,
+        c.first_name,
+        c.last_name,
+        MIN(o.order_date) AS first_order,
+        MAX(o.order_date) AS most_recent_order,
+        COUNT(o.id) AS number_of_orders
+    FROM {{ source('hevo_schema', 'raw_customers') }} c
+    LEFT JOIN {{ source('hevo_schema', 'raw_orders') }} o
+    ON c.id = o.user_id
+    GROUP BY c.id, c.first_name, c.last_name
 ),
 customer_lifetime_value AS (
     SELECT
         o.user_id AS customer_id,
         SUM(p.amount) AS customer_lifetime_value
-    FROM {{ source('<SCHEMA>', 'raw_orders') }} o
-    LEFT JOIN {{ source('<SCHEMA>', 'raw_payments') }} p
+    FROM {{ source('hevo_schema', 'raw_orders') }} o
+    LEFT JOIN {{ source('hevo_schema', 'raw_payments') }} p
     ON o.id = p.order_id
     GROUP BY o.user_id
 )
